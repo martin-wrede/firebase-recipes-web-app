5c240456b47094a93f16be33adb4916643677219
martin-wrede <martin.wrede@targetx.de>
2023-03-13 16:39:26 +0100

    added env format data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 
+import { useState } from "react"
+import FirebaseAuthService from "./FirebaseAuthService"
+import LoginForm from "./components/LoginForm"
 import './App.css'
-import firebase from './FirebaseConfig'
+
+
+ import firebase from './FirebaseConfig'
 
 
 function App() {
- 
+  
+  const [user, setUser] = useState(null)
+
+  FirebaseAuthService.subscribeToAuthChanges(setUser)
+
   return (
     <div className="App">
     <div  className="title-row">
     <h1  className="title">Firebase Recipe</h1>
+    <LoginForm existingUser={user}></LoginForm>
     </div>
     </div>
   )
--- src/FirebaseConfig.jsx
+++ src/FirebaseConfig.jsx
@@ -10,20 +10,21 @@ const config = {
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
-
 */
-const config = {
-    apiKey: "AIzaSyAFeYP2kb7SjkasLqR8eYmal1xW_VQMaaI",
-    authDomain: "fir-recipes-9a215.firebaseapp.com",
-    projectId: "fir-recipes-9a215",
-    storageBucket: "fir-recipes-9a215.appspot.com",
-    messagingSenderId: "552004081965",
-    appId: "1:552004081965:web:6d67599261fe6598ad9ba4",
-    measurementId: "G-M2ZDV4Y4Q1"
-  };
 
-if(!firebase.apps.length){
-    firebase.initializeApp(config)
+
+const config = {
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
 }
 
-export default firebase
+export default firebase;
